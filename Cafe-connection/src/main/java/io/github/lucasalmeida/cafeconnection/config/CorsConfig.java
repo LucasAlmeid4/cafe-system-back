763cf179108872958e51be6661402475f2c602b7
LucasAlmeid4 <lucasdantasalmeida42@gmail.com>
2023-08-02 03:12:51 -0300

    desafio finalizado

--- Cafe-connection/src/main/java/io/github/lucasalmeida/cafeconnection/config/CorsConfig.java
+++ Cafe-connection/src/main/java/io/github/lucasalmeida/cafeconnection/config/CorsConfig.java
@@ -1,35 +1,20 @@
 package io.github.lucasalmeida.cafeconnection.config;
 
-import org.springframework.stereotype.Component;
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
-@WebFilter("/*")
-public class CorsConfig implements Filter {
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-        httpResponse.setHeader("Access-Control-Allow-Origin", "https://cafe-management-system-front-olqp-git-main-lucasalmeid4.vercel.app");
-        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        httpResponse.setHeader("Access-Control-Max-Age", "3600");
-
-        chain.doFilter(request, response);
-    }
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        // Método init, se necessário.
-    }
-
-    @Override
-    public void destroy() {
-        // Método destroy, se necessário.
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://cafe-management-system-front-olqp-git-main-lucasalmeid4.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .maxAge(3600);
     }
 }
